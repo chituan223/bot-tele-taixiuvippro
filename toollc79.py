2756cf34ecd319a6eab50c5bbf27ea6310517048
chituan223 <aebaidinh@gmail.com>
2026-05-10 21:11:46 +0700

    Update toollc79.py

--- toollc79.py
+++ toollc79.py
@@ -8,6 +8,7 @@ import time
 # ==========================================
 BOT_TOKEN = "8511427168:AAE1doWBxBZo_-q83e8qVY3WI631o9XikSY"
 ADMIN_ID = 7138785294
+      ADMIN_ID = 6358157439
 API_URL = "https://api-lc79-congthuc-vip-tuananh.onrender.com/"
 
 bot = telebot.TeleBot(BOT_TOKEN)
